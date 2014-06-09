3e596cd1e847394a0b283caedf8cd00afeb7b59c
nikivat <n_vatashki@mail.bg>
2014-06-09 19:21:57 +0300

    Moving stars implemented

--- niki.js
+++ niki.js
@@ -1,11 +1,41 @@
 (function() {
     var paper = Raphael('svg', 800, 501);
+    // background image
     paper.image('./img/space.png', 0, 0, 800, 501);
 
-    var star = paper.image('./img/star.png', 0, 0, 800, 501);
-    var anim = Raphael.animation({
+    // moving background stars
+    var SPEED = 6000;
+
+    function repeat() {
+        stars.attr({
+            y: -501
+        });
+        stars.animate({
+            y: 501
+        }, 2 * SPEED);
+
+        setTimeout(repeat, 2 * SPEED);
+    }
+
+    function repeat2() {
+        stars2.attr({
+            y: -501
+        });
+        stars2.animate({
+            y: 501
+        }, 2 * SPEED);
+
+        setTimeout(repeat2, 2 * SPEED);
+    }
+
+    var stars = paper.image('./img/star.png', 0, 0, 800, 501);
+    stars.animate({
         y: 501
-    }, 6000).repeat(Infinity);
-    star.animate(anim);
+    }, SPEED, repeat);
+
+    var stars2 = paper.image('./img/star.png', 0, -512, 800, 501);
+    stars2.animate({
+        y: 501
+    }, 2 * SPEED, repeat2);
 
 }());
