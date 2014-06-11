d71b0df02659dee33f546c25e2cdb5abe67b5574
nikivat <n_vatashki@mail.bg>
2014-06-11 09:47:46 +0300

    Some fixes

--- niki.js
+++ niki.js
@@ -1,17 +1,17 @@
 (function() {
-    var paper = Raphael('svg', 800, 501);
+    var paper = Raphael('svg', 313, 500);
     // background image
-    paper.image('./img/space.png', 0, 0, 800, 501);
+    paper.image('./img/space.png', 0, 0, 313, 500);
 
     // moving background stars
     var SPEED = 6000;
 
     function repeat() {
         stars.attr({
-            y: -501
+            y: -500
         });
         stars.animate({
-            y: 501
+            y: 500
         }, 2 * SPEED);
 
         setTimeout(repeat, 2 * SPEED);
@@ -19,50 +19,23 @@
 
     function repeat2() {
         stars2.attr({
-            y: -501
+            y: -500
         });
         stars2.animate({
-            y: 501
+            y: 500
         }, 2 * SPEED);
 
         setTimeout(repeat2, 2 * SPEED);
     }
 
-    var stars = paper.image('./img/star.png', 0, 0, 800, 501);
+    var stars = paper.image('./img/star.png', 0, 0, 313, 500);
     stars.animate({
-        y: 501
+        y: 500
     }, SPEED, repeat);
 
-    var stars2 = paper.image('./img/star.png', 0, -512, 800, 501);
+    var stars2 = paper.image('./img/star.png', 0, -500, 313, 500);
     stars2.animate({
-        y: 501
+        y: 500
     }, 2 * SPEED, repeat2);
-
-
-    // spaceship
-    var shipStage = new Kinetic.Stage({
-        container: 'ship',
-        width: 800,
-        height: 501
-    });
-
-    var shipLayer = new Kinetic.Layer();
-
-
-    var shipImg = new Image();
-    shipImg.src = 'img/spaceship.png';
-    var image = new Kinetic.Image({
-        x: 380,
-        y: 400,
-        image: shipImg,
-        width: 38,
-        height: 76
-    });
-
-
-    shipLayer.add(image);
-    shipStage.add(shipLayer);
-
-
 
 }());
